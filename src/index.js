e0662133b229a131012e675aba9012e25dc29eac
bombi <bombic3@naver.com>
2022-08-04 16:15:51 +0900

    데이터 로딩

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, legacy_createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from 'redux-thunk';
+import rootReducer from "./modules";
+
+const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  
   document.getElementById("root")
 );
